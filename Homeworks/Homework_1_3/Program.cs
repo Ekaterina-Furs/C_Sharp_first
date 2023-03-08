9d3bf4e0251d7d0033954e537c1cabd9584ec80e
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-09 01:34:17 +0400

    Homework_1_3_Changed

--- Homeworks/Homework_1_3/Program.cs
+++ Homeworks/Homework_1_3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the number");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a%2 == 0)
+{
+    Console.Write("Even");
+}
+else
+{
+    Console.Write("Odd");
+}
