b20feff033bdd0603a3f30ff753150a133968a38
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-09 00:02:10 +0400

    Homework_1_1

--- Homeworks/Homework_1_1/Program.cs
+++ Homeworks/Homework_1_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the first number");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the second number");
+int b = int.Parse(Console.ReadLine()!);
+
+if(a > b)
+{
+    Console.Write("max=");
+    Console.WriteLine(a);
+    Console.Write("min=");
+    Console.WriteLine(b);
+}
+
+else
+{   
+    Console.Write("max=");
+    Console.WriteLine(b);
+    Console.Write("min=");
+    Console.WriteLine(a);
+}
+
+if (a == b)
+{
+    
+}
