3e0d1e934eb174924f0cce7e526c80b62f966cba
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-10 14:47:34 +0400

    Lesson_2_0

---
+++ Lesson_2/Lesson_2_0/Program.cs
@@ -0,0 +1,10 @@
+﻿int TakeNum (int num)
+{
+    Console.WriteLine(num);
+    return num % 10;
+}
+
+int th_num = new Random().Next(100, 1000);
+TakeNum(th_num);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
