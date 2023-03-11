c2d7ff5c42002d8247c37a6a691dfd58d1195540
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-11 16:19:54 +0400

    Homework_2_1

---
+++ Homeworks_2/Homework_2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+int a = new Random().Next(100, 1000);
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 10;
+    int num2 = num1 % 10;
+    return num2;
+}
+TakeNum(a);
+int result = TakeNum(a);
+Console.WriteLine(result);
