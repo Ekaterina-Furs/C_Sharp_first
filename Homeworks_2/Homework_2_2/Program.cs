129a29823053246ce986c40fd31fd1013af46b29
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-11 16:38:29 +0400

    Homework_2_2

---
+++ Homeworks_2/Homework_2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит слуайное трехзначное число и удаляет вторую цифру этого числа
+
+int a = new Random().Next(100, 1000);
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 100;
+    int num2 = num % 10;
+    int num3 = num1*10 + num2;
+    return num3;
+}
+TakeNum(a);
+int result = TakeNum(a);
+Console.WriteLine(result);
+
