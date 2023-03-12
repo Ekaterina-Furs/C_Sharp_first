58f89d8e2a7269ed15319af03b97d809baa5c417
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-12 23:28:58 +0400

    Homework_2_3

--- Homeworks_2/Homework_2_3/Program.cs
+++ Homeworks_2/Homework_2_3/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+Console.WriteLine("Enter the number");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a < 100)
+{
+    Console.WriteLine("The third number doesn't exist");
+}
+else
+{
+    if(a < 999) a = a % 10;
+    Console.WriteLine(a);
+}
+
+    
+
+
 
