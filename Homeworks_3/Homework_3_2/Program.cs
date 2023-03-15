10b1a5baf530e02940dbeeed641d29cc89d92619
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-16 00:33:04 +0400

    Homework_3_2

---
+++ Homeworks_3/Homework_3_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// 3. Напишите программу, которая принимает на вход число (N) 
+//    и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Ввeдите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+void Cube(int c)
+{
+    int num = 1;
+    while (num <= c)
+    {
+        Console.WriteLine(num * num * num);
+        num ++;
+    }
+}
+
+Cube(N);
