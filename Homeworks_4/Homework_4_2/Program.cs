41846d780ead87380e6eabf5b4e677d5c2766438
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-19 23:02:47 +0400

    Homework_4_2

---
+++ Homeworks_4/Homework_4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+
+Console.WriteLine("Enter number  ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+while(number > 0)
+{
+    int digit = number % 10;
+    number = number / 10;
+    sum = sum + digit;
+}
+
+Console.WriteLine($"The sum of the digits is {sum}");
