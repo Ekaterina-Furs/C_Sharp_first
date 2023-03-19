9d14daa614fe8110b1fc88b596e98ead0671f3ce
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-19 22:39:08 +0400

    Homework_4_1

---
+++ Homeworks_4/Homework_4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход числа A и B и возводит число A в натуральную степень B.
+
+Console.WriteLine("Enter number A");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter number B");
+int B = int.Parse(Console.ReadLine()!);
+
+
+
+int Degree(int a, int b)
+{
+    int res = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        res = res * a;
+    }
+    return res;
+}
+Degree(A,B);
+int result = Degree(A, B);
+Console.WriteLine($"The result is {result}");
+
