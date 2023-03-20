b2f504114040993f018b710bd400232c72b36262
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-21 00:44:37 +0400

    Homework_4_3

--- Homeworks_4/Homework_4_3/Program.cs
+++ Homeworks_4/Homework_4_3/Program.cs
@@ -1,3 +1,30 @@
 ﻿// Напишите программу , которая задает массив из 8 элементов случайными числами и выводит их на экран.
 // Оформите заполнение массива и вывод в виде функции.
 
+Console.WriteLine("Задайте размер массива");
+
+int num = int.Parse(Console.ReadLine()!);
+int[] array = new int [num];
+
+void FillArray(int[] arr)
+{
+    int[] array = new int [num];
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = new Random().Next(0,100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int n = arr.Length;
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+    Console.WriteLine();
+}
+
+FillArray(array);
+PrintArray(array);
