ab734a4eef6e794a420b00bad6248b215a62a95d
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-27 22:24:22 +0400

    Corrections

--- Homeworks_6/Homework_6_1/Program.cs
+++ Homeworks_6/Homework_6_1/Program.cs
@@ -1,31 +1,20 @@
 ﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-Console.Write("введите количество элементов:");
-int m = int.Parse(Console.ReadLine()!);
-int[] array = new int[m];
-
-int[] FillArray(int size)
-{
-    int[] arr = new int[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = int.Parse(Console.ReadLine()!);
-        Console.Write($"{arr[i]}  ");
-    }
-    return arr;
-}
-
-void PrintArray(int[] arr)
-{
-    int size = arr.Length;
-    for (int i = 0; i < size; i++)
-    {
-        Console.Write($"{arr[i]} " );
-    } 
-    Console.WriteLine();   
-}
-
-int[] order = FillArray(m);
-PrintArray(order);
+Console.Write("Введите числа через пробел: ");
+string array = Console.ReadLine()!;
+Console.Write(array);
 
 
+
+
+
+//void Counter(int[] arr)
+//{
+ //   int c = 0;
+ //   for (int i = 0; i < arr.Length; i++)
+  //  {
+ ///       if(arr[i] > 0) c++;
+  //  }
+  //  Console.WriteLine($"The number of elements > 0 is {c}");
+//}
+
+//Counter(array);
