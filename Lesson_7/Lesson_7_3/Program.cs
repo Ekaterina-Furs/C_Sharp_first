196f58c7f6522e5e4c92d0e1908d32b5617bbbb2
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-28 15:30:41 +0400

    Lesson_7

---
+++ Lesson_7/Lesson_7_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+//    
+
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillMatrix(int[,] matr, int from, int to)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(from, to + 1);
+        }
+    }
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine()!);
+Console.Write("The range of random numbers WITH -> ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("The range of random numbers UP TO -> ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = new int[row, column];
+
+
+FillMatrix(matrix, start, stop);
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
