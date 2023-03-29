7a07ba34800393f73b66cdd894b5b27e7461047a
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-29 22:43:01 +0400

    Homeworks_6_final

--- Homeworks_6/Homework_6_1/Program.cs
+++ Homeworks_6/Homework_6_1/Program.cs
@@ -1,20 +1,20 @@
 ﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-Console.Write("Введите числа через пробел: ");
-string array = Console.ReadLine()!;
-Console.Write(array);
+
+int Counter(int number)
+{
+    int count = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write($"The number {i}: ");
+        int a = int.Parse(Console.ReadLine()!);
+        if(a > 0) count++;
+}
+        return count;
+}
+Console.WriteLine("Enter the number:  ");
+int n = int.Parse(Console.ReadLine()!);
+int result = Counter(n);
+Console.WriteLine($"The number > 0 is {result}");
 
 
 
-
-
-//void Counter(int[] arr)
-//{
- //   int c = 0;
- //   for (int i = 0; i < arr.Length; i++)
-  //  {
- ///       if(arr[i] > 0) c++;
-  //  }
-  //  Console.WriteLine($"The number of elements > 0 is {c}");
-//}
-
-//Counter(array);
--- Homeworks_6/Homework_6_2/Program.cs
+++ Homeworks_6/Homework_6_2/Program.cs
@@ -2,21 +2,28 @@
 //y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 Console.Write("Enter k1: ");
-int k1 = int.Parse(Console.ReadLine()!);
+double k1 = double.Parse(Console.ReadLine()!);
 Console.Write("Enter b1: ");
-int b1 = int.Parse(Console.ReadLine()!);
+double b1 = double.Parse(Console.ReadLine()!);
 Console.Write("Enter k2: ");
-int k2 = int.Parse(Console.ReadLine()!);
+double k2 = double.Parse(Console.ReadLine()!);
 Console.Write("Enter b2: ");
-int b2 = int.Parse(Console.ReadLine()!);
- 
+double b2 = double.Parse(Console.ReadLine()!);
 
-int x = -(b1 - b2) / (k1 - k2);
-int y = k1 * x + b1;
- 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
- 
+void InterSec(double k_1, double k_2, double b_1, double b_2)
+{
+    double k_sub = k_1 - k_2;
+    if (k_sub != 0)
+    {
+        double x = (b_2 - b_1) / (k_1 - k_2);
+        double y = k_1 * x + b_1;
+        Console.WriteLine($"The point of intersection: ({x},{y})");
+    }
+    else if (k1 == k2 && b1 == b2)
+        Console.WriteLine("The infinitive number of intersection points");
+    else
+        Console.WriteLine("There is no intersection point");
+    
+}
 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
-
+InterSec(k1, k2, b1, b2);
