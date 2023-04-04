71680f3d321783a7a397dd4e10a867719b2f3ad2
EkaterinaFurs <mrs-katrina@mail.ru>
2023-04-04 16:31:01 +0400

    Homework_9_1

--- Homeworks_9/Homework_9_1/Program.cs
+++ Homeworks_9/Homework_9_1/Program.cs
@@ -3,3 +3,16 @@
 //M = 1; N = 5 -> "2, 4"
 //M = 4; N = 8 -> "4, 6, 8"
 
+Console.Write("Enter number M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+void EvenArray(int M, int N)
+{
+    if (M > N) return;
+    if (M % 2 == 0) Console.Write($"{M} ");
+    EvenArray(M + 1, N);
+}
+EvenArray(m, n);
+
