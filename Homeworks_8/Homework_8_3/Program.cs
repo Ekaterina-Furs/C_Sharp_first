1401e0337bbc6c3ace5f660638c0cfb95fdcf108
EkaterinaFurs <mrs-katrina@mail.ru>
2023-04-15 21:04:31 +0400

    Homework_8_3_corrections

--- Homeworks_8/Homework_8_3/Program.cs
+++ Homeworks_8/Homework_8_3/Program.cs
@@ -39,43 +39,49 @@ void FillMatrix(int[,] matr, int from, int to)
 int[,] MatrixMultiplicacion(int[,] firstArray, int[,] secondArray)
 {
     int row1 = firstArray.GetLength(0);
-    int column1 =  firstArray.GetLength(1);
+    int column1 = firstArray.GetLength(1);
 
     int row2 = secondArray.GetLength(0);
-    int column2 =  secondArray.GetLength(1);
+    int column2 = secondArray.GetLength(1);
 
     int[,] resultmatrix = new int[row1, column2];
 
-    if(column1 != row2) return resultmatrix;
-    else if(column1 == row2)
-    {
+    if (column1 != row2) return resultmatrix;
+    else if (column1 == row2)
         resultmatrix = new int[row1, column2];
-    }
-        
 
-    for(int i=0; i < row1; i++)
+
+    for (int i = 0; i < row1; i++)
     {
-        for(int j =0; j <column2; j++)
+        for (int j = 0; j < column2; j++)
         {
-            for(int p=0; p < column1; p++)
-//берем строку первой матрицы и столбец второй
-//перемножаем элменты, собираем сумму произведений.
-
-            resultmatrix[i,j] += firstArray[i,p] * secondArray[p, j];
+            for (int p = 0; p < column1; p++)
+                resultmatrix[i, j] += firstArray[i, p] * secondArray[p, j];
         }
     }
-return resultmatrix;
+    return resultmatrix;
 }
 
-Console.WriteLine("The random matrix is:");
+Console.WriteLine("The first random matrix is:");
 FillMatrix(matrix, start, stop);
 PrintMatrix(matrix);
 Console.WriteLine();
 
-Console.WriteLine("The random matrix is:");
-FillMatrix(matrix, start, stop);
-PrintMatrix(matrix);
+
+Console.Write("Enter the number of rows: ");
+int row_1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_1 = int.Parse(Console.ReadLine()!);
+Console.Write("The range of random numbers WITH -> ");
+int start_1 = int.Parse(Console.ReadLine()!);
+Console.Write("The range of random numbers UP TO -> ");
+int stop_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int[,] matrix_1 = new int[row_1, column_1];
+
+Console.WriteLine("The second random matrix is:");
+FillMatrix(matrix_1, start_1, stop_1);
+PrintMatrix(matrix_1);
 Console.WriteLine();
 
-MatrixMultiplicacion(matrix, matrix);
-Console.WriteLine();
+Console.WriteLine(MatrixMultiplicacion(matrix, matrix_1));
